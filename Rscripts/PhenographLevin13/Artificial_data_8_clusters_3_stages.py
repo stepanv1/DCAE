2203b6407462cce4e3c101a695989b4f998377ce
stepanv1 <stepanv1@gmail.com>
2021-02-24 18:15:11 -0500

    Redefining artificial set, potential research

--- Rscripts/PhenographLevin13/Artificial_data_8_clusters_3_stages.py
+++ Rscripts/PhenographLevin13/Artificial_data_8_clusters_3_stages.py
@@ -49,7 +49,7 @@ def frange_anneal(n_epoch, ratio=0.25, shape='sin'):
                 L[c] = np.sqrt(c)/norm
             if shape=='sin':
                 Om = (np.pi/2/(n_epoch*ratio))
-                L[c] =  np.sin(Om*c)**6
+                L[c] =  np.sin(Om*c)**4
         else:
             L[c]=1
     return L
@@ -82,202 +82,15 @@ perp.argtypes = [ndpointer(ctypes.c_double, flags="C_CONTIGUOUS"),
 
 d= 5
 # subspace clusters centers
-original_dim = 15
-d= 5 # main informative dimensions
-sep=3
-cl1_center = np.zeros(original_dim)
-cl2_center = np.concatenate((sep*np.ones(1), np.zeros(original_dim-1)), axis=0 )
-cl3_center = np.concatenate((2*sep*np.ones(1), np.zeros(original_dim-1)), axis=0 )
-cl4_center = np.concatenate((3*sep*np.ones(1), np.zeros(original_dim-1)), axis=0 )
-cl5_center = np.concatenate((4*sep*np.ones(1), np.zeros(original_dim-1)), axis=0 )
-cl6_center = np.concatenate((3*sep*np.ones(1), np.zeros(original_dim-1)), axis=0 )
-cl6_center[1] = sep
-cl7_center = np.concatenate((4*sep*np.ones(1), np.zeros(original_dim-1)), axis=0 )
-cl7_center[2] = sep
-# add big cluster of 'irrelevant' cells with very different expression set
-cl8_center  = np.concatenate((np.zeros(4), 0.5*sep*np.ones(1), np.zeros(original_dim-9), np.ones(4)), axis=0)
-
-# cluster populatiosn
-ncl1 = ncl2 = ncl3 = ncl4  = ncl5 = ncl6 = ncl7 = 6000
-ncl8 = 20000
-# cluster labels
-lbls = np.concatenate((np.zeros(ncl1), np.ones(ncl2), 2*np.ones(ncl3), 3*np.ones(ncl4), 4*np.ones(ncl5),
-                       5*np.ones(ncl6), 6*np.ones(ncl7), -7*np.ones(ncl8)), axis=0)
-#introduce correlation
-
-from sklearn import datasets
-r = sklearn.datasets.make_spd_matrix(d,  random_state=12346)
-r8 = sklearn.datasets.make_spd_matrix(d,  random_state=12347)
-
-
-# Generate the random samples.
-y1 = np.random.multivariate_normal(cl1_center[:d], r, size=ncl1)
-y2 = np.random.multivariate_normal(cl2_center[:d], r, size=ncl2)
-y3 = np.random.multivariate_normal(cl3_center[:d], r, size= ncl3)
-y4 = np.random.multivariate_normal(cl4_center[:d], r, size=ncl4)
-y5 = np.random.multivariate_normal(cl5_center[:d], r, size=ncl5)
-y6 = np.random.multivariate_normal(cl6_center[:d], r, size=ncl6)
-y7 = np.random.multivariate_normal(cl7_center[:d], r, size=ncl7)
-y8 = np.random.multivariate_normal(cl8_center[np.concatenate((np.zeros(4), np.ones(1), np.zeros(original_dim-9), np.ones(4))).astype('bool')], r, size=ncl8)
-
-#wd= 0.3
-cl1 = np.concatenate([y1, np.zeros((ncl1,original_dim - d))], axis=1 )
-cl2 = np.concatenate([y2, np.zeros((ncl2,original_dim - d))], axis=1 )
-cl3 = np.concatenate([y3, np.zeros((ncl3,original_dim - d))], axis=1 )
-cl4 = np.concatenate([y4, np.zeros((ncl4,original_dim - d))], axis=1 )
-cl5 = np.concatenate([y5, np.zeros((ncl5,original_dim - d))], axis=1 )
-cl6 = np.concatenate([y6, np.zeros((ncl6,original_dim - d))], axis=1 )
-cl7 = np.concatenate([y7, np.zeros((ncl7,original_dim - d))], axis=1 )
-cl8 =  np.zeros((ncl8,original_dim ))
-cl8[:,np.concatenate((np.zeros(4), np.ones(1), np.zeros(original_dim-9), np.ones(4))).astype('bool')] = y8
-
-figv1 = plt.figure();
-sns.violinplot(data= cl1, bw = 0.1);plt.show()
-figv2 = plt.figure();
-sns.violinplot(data= cl2, bw = 0.1);plt.show()
-figv3 = plt.figure();
-sns.violinplot(data= cl8, bw = 0.1);plt.show()
-
-#sns.violinplot(data= cl1, bw = 0.1);sns.violinplot(data= cl8, bw = 0.1);
-#noisy or not:
-#noise_sig1 =  np.concatenate((np.zeros(20),  np.ones(10)), axis=0 )
-#noise_sig2 = np.concatenate((np.ones(10), np.zeros(20)), axis=0 )
-# add noise to orthogonal dimensions
-noise_scale =0.2
-cl1_noisy = cl1 + np.concatenate([np.zeros((ncl1,d)), np.abs(np.random.normal(loc=0, scale = noise_scale, size=(ncl1,original_dim - d)))], axis=1 )
-cl2_noisy = cl2 + np.concatenate([np.zeros((ncl2,d)), np.abs(np.random.normal(loc=0, scale = noise_scale, size=(ncl2,original_dim - d)))], axis=1 )
-cl3_noisy = cl3 + np.concatenate([np.zeros((ncl3,d)), np.abs(np.random.normal(loc=0, scale = noise_scale, size=(ncl3,original_dim - d)))], axis=1 )
-cl4_noisy = cl4 + np.concatenate([np.zeros((ncl4,d)), np.abs(np.random.normal(loc=0, scale = noise_scale, size=(ncl4,original_dim - d)))], axis=1 )
-cl5_noisy = cl5 + np.concatenate([np.zeros((ncl5,d)), np.abs(np.random.normal(loc=0, scale = noise_scale, size=(ncl5,original_dim - d)))], axis=1 )
-
-cl6_noisy = cl6 + np.concatenate([np.zeros((ncl6,d)), np.abs(np.random.normal(loc=0, scale = noise_scale, size=(ncl6,original_dim - d)))], axis=1 )
-cl7_noisy = cl7 + np.concatenate([np.zeros((ncl7,d)), np.abs(np.random.normal(loc=0, scale = noise_scale, size=(ncl7,original_dim - d)))], axis=1 )
-cl8_noisy = cl8
-cl8_noisy[:, ~np.concatenate((np.zeros(4), np.ones(1), np.zeros(original_dim-9), np.ones(4))).astype('bool')] = \
-    np.abs(np.random.normal(loc=0, scale = noise_scale, size=(ncl8,original_dim - d)))
-figv2 = plt.figure();
-sns.violinplot(data= cl1_noisy, bw = 0.1);plt.show()
-figv3 = plt.figure();
-sns.violinplot(data= cl2_noisy, bw = 0.1);plt.show()
-figv3 = plt.figure();
-sns.violinplot(data= cl4_noisy, bw = 0.1);plt.show()
-figv1 = plt.figure();
-sns.violinplot(data= cl8_noisy, bw = 0.1);plt.show()
-
-
-#sns.violinplot(data= cl1_noisy, bw = 0.1);
-#sns.violinplot(data= cl2_noisy, bw = 0.1);
-#sns.violinplot(data= cl3_noisy, bw = 0.1);
-#sns.violinplot(data= cl4_noisy, bw = 0.1);
-#sns.violinplot(data= cl5_noisy, bw = 0.1);
-#sns.violinplot(data= cl6_noisy, bw = 0.1);
-#sns.violinplot(data= cl7_noisy, bw = 0.1);
-
-noisy_clus = np.concatenate((cl1_noisy, cl2_noisy, cl3_noisy, cl4_noisy, cl5_noisy, cl6_noisy,
-                             cl7_noisy, cl8_noisy), axis=0)
-
-scaler = MinMaxScaler(copy=False, feature_range=(0, 1))
-data =  scaler.fit_transform(noisy_clus)
-mapper = umap.UMAP(n_neighbors=30, n_components=2, metric='euclidean', random_state=42, min_dist=0, low_memory=True).fit(data)
-y =  mapper.transform(data)
-cdict = {0: 'red', 1: 'blue', 2: 'green'}
-#plt.scatter(y[:, 0], y[:, 1], c=color, s=1., cmap=plt.cm.Spectral)
-dataset = pd.DataFrame()
-dataset['x'] = y[:, 0]
-dataset['y'] = y[:, 1]
-dataset['color'] = [str(x) for x in lbls]
-#pca
-from sklearn import decomposition
-pca = decomposition.PCA(n_components=2)
-pca.fit(data)
-yp = pca.transform(data)
-#plt.scatter(y[:, 0], y[:, 1], c=color, s=1., cmap=plt.cm.Spectral)
-dataset = pd.DataFrame()
-dataset['x'] = yp[:, 0]
-dataset['y'] = yp[:, 1]
-dataset['color'] = [str(x) for x in lbls]
-
-
-
-fig = plot2D_cluster_colors(y, lbls=lbls, msize=5)
-fig.show()
-fig = plot2D_cluster_colors(yp, lbls=lbls, msize=5)
-fig.show()
+original_dim = 30
 # preprocess before the deep
-source_dir = '/media/grines02/vol1/Box Sync/Box Sync/CyTOFdataPreprocess/simulatedData'
-output_dir  = '/media/grines02/vol1/Box Sync/Box Sync/CyTOFdataPreprocess/simulatedData/output'
-
-sns.violinplot(data= data[lbls==0.0, :], bw = 0.1);
-sns.violinplot(data= noisy_clus[lbls==-7.0, :], bw = 0.1);
 
 
 k = 30
 k3 = k * 3
-'''
-aFrame = noisy_clus
-aFrame.shape
-# set negative values to zero
-#aFrame[aFrame < 0] = 0
-#randomize order
-#IDX = np.random.choice(aFrame.shape[0], aFrame.shape[0], replace=False)
-#patient_table = patient_table[IDX,:]
-#aFrame= aFrame[IDX,:]
-#lbls = lbls[IDX]
-len(lbls)
-scaler = MinMaxScaler(copy=False, feature_range=(0, 1))
-scaler.fit_transform(aFrame)
-nb=find_neighbors(aFrame, k3, metric='euclidean', cores=12)
-Idx = nb['idx']; Dist = nb['dist']
-#Dist = Dist[IDX]
-#Idx = Idx[IDX]
-nrow=Idx.shape[0]
-# find nearest neighbours
-def singleInput(i):
-    nei = noisy_clus[Idx[i, :], :]
-    return [nei, i]
-# find nearest neighbours
-nn=30
-rk=range(k3)
-def singleInput(i):
-     nei =  aFrame[Idx[i,:],:]
-     di = [np.sqrt(sum(np.square(aFrame[i] - nei[k_i,]))) for k_i in rk]
-     return [nei, di, i]
-nrow = len(lbls)
-inputs = range(nrow)
-from joblib import Parallel, delayed
-from pathos import multiprocessing
-num_cores = 12
-#pool = multiprocessing.Pool(num_cores)
-results = Parallel(n_jobs=num_cores, verbose=0, backend="threading")(delayed(singleInput, check_pickle=False)(i) for i in inputs)
-
-neibALL = np.zeros((nrow, k3, original_dim))
-Distances = np.zeros((nrow, k3))
-neib_weight = np.zeros((nrow, k3))
-Sigma = np.zeros(nrow, dtype=float)
-for i in range(nrow):
-    neibALL[i,] = results[i][0]
-for i in range(nrow):
-    Distances[i,] = results[i][1]
-#Compute perpelexities
-nn=30
-perp((Distances[:,0:k3]),       nrow,     original_dim,   neib_weight,          nn,          k3,   Sigma,    12)
-      #(     double* dist,      int N,    int D,       double* P,     double perplexity,    int K, int num_threads)
-np.shape(neib_weight)
-plt.plot(neib_weight[1,])
-#sort and normalise weights
-topk = np.argsort(neib_weight, axis=1)[:,-nn:]
-topk= np.apply_along_axis(np.flip, 1, topk,0)
-neib_weight=np.array([ neib_weight[i, topk[i]] for i in range(len(topk))])
-neib_weight=sklearn.preprocessing.normalize(neib_weight, axis=1, norm='l1')
-neibALL=np.array([ neibALL[i, topk[i,:],:] for i in range(len(topk))])
-plt.plot(neib_weight[1,:]);plt.show()
-outfile = source_dir + '/8art_scaled_cor_62000_15D.npz'
-np.savez(outfile, aFrame = aFrame, Idx=Idx, lbls=lbls,  Dist=Dist,
-         neibALL=neibALL, neib_weight= neib_weight, Sigma=Sigma)
-'''
 source_dir = '/media/grines02/vol1/Box Sync/Box Sync/CyTOFdataPreprocess/simulatedData'
 output_dir  = '/media/grines02/vol1/Box Sync/Box Sync/CyTOFdataPreprocess/simulatedData/output'
-outfile = source_dir + '/8art_scaled_cor_62000.npz'
+outfile = source_dir + '/8art_scaled_cor_62000_15D.npz'
 k=30
 markers = np.arange(30).astype(str)
 # np.savez(outfile, weight_distALL=weight_distALL, cut_neibF=cut_neibF,neibALL=neibALL)
@@ -305,14 +118,14 @@ tf.compat.v1.disable_eager_execution()
 # targetTr = np.repeat(aFrame, r, axis=0)
 k = 30
 k3 = k * 3
-coeffCAE = 5
-epochs = 15000
-ID = 'Art8'+ str(coeffCAE) + '_' + str(epochs) + '_corrected_correlated_weighted_CAE_weigthed_MMD_highMMD_smols_sd_62000'
+coeffCAE = 10
+epochs = 500
+ID = 'Art8'+ str(coeffCAE) + '_' + str(epochs) + '_corrected_correlated_weighted_CAE_weighted_MMD'
 # TODO try downweight mmd to the end of computation
 #DCAE_weight = K.variable(value=0)
 #DCAE_weight_lst = K.variable(np.array(frange_anneal(epochs, ratio=0)))
 MMD_weight = K.variable(value=0)
-MMD_weight_lst = K.variable(np.array(frange_anneal(epochs, ratio=0.2)))
+MMD_weight_lst = K.variable(np.array(frange_anneal(epochs, ratio=0.1)))
 
 nrow = aFrame.shape[0]
 batch_size = 256
@@ -349,6 +162,37 @@ normSigma = nrow / sum(1 / Sigma)
 
 lam=1e-4
 
+
+def DCAE3D_loss(x, x_decoded_mean):  # attempt to avoid vanishing derivative of sigmoid
+    U = K.variable(value=encoder.get_layer('intermediate').get_weights()[0])  # N x N_hidden
+    W = K.variable(value=encoder.get_layer('intermediate2').get_weights()[0])  # N x N_hidden
+    Z = K.variable(value=encoder.get_layer('z_mean').get_weights()[0])  # N x N_hidden
+    U = K.transpose(U);
+    W = K.transpose(W);
+    Z = K.transpose(Z);  # N_hidden x N
+
+    u = encoder.get_layer('intermediate').output
+    du = tf.linalg.diag((tf.math.sign(u) + 1) / 2)
+    m = encoder.get_layer('intermediate2').output
+    dm = tf.linalg.diag((tf.math.sign(m) + 1) / 2)  # N_batch x N_hidden
+    s = encoder.get_layer('z_mean').output
+    #r = tf.linalg.einsum('aj->a', s ** 2)
+    ds = tf.linalg.diag(tf.math.scalar_mul(0, s)+1)
+
+    S_0W = tf.einsum('akl,lj->akj', du, U)
+    S_1W = tf.einsum('akl,lj->akj', dm, W)  # N_batch x N_input ??
+    # tf.print((S_1W).shape) #[None, 120]
+    S_2Z = tf.einsum('akl,lj->akj', ds, Z)  # N_batch ?? TODO: use tf. and einsum and/or tile
+    # tf.print((S_2Z).shape)
+    diff_tens = tf.einsum('akl,alj->akj', S_2Z, S_1W)  # Batch matrix multiplication: out[a,i,k] = sum_j s[a,i,j] * t[a, j, k]
+    diff_tens = tf.einsum('akl,alj->akj', diff_tens, S_0W)
+    # tf.Print(K.sum(diff_tens ** 2))
+    return 1 / normSigma * (SigmaTsq) * lam * (K.sum(diff_tens ** 2))
+
+def pot(alp, x):
+    return np.select([(x < alp),(x >= alp)*(x<=1), x>1 ], [10, 0, 10])
+
+alp=0.2
 def DCAE_loss(x, x_decoded_mean):  # attempt to avoid vanishing derivative of sigmoid
     U = K.variable(value=encoder.get_layer('intermediate').get_weights()[0])  # N x N_hidden
     W = K.variable(value=encoder.get_layer('intermediate2').get_weights()[0])  # N x N_hidden
@@ -362,20 +206,23 @@ def DCAE_loss(x, x_decoded_mean):  # attempt to avoid vanishing derivative of si
     m = encoder.get_layer('intermediate2').output
     dm = tf.linalg.diag((tf.math.sign(m)+1)/2)  # N_batch x N_hidden
     s = encoder.get_layer('z_mean').output
-    #ds = K.sqrt(tf.linalg.diag(s * (1 - s)))  # N_batch x N_hidden
-    #ds = tf.linalg.diag(tf.math.sign(s*(1-s) ** 2 +0.1))
-    #bs = np.shape(s)[0]
-    #tf.print(bs)  # [None, 120]
+
     r = tf.linalg.einsum('aj->a', s**2)
-    #tf.print(r.shape)
-    #b_i = tf.eye(latent_dim)
-    #tf.print(tf.shape(b_i))
-    #ds = tf.einsum('alk,a ->alk', b_i,r)
-    ds = MMD_weight * (-2 * r + 1.5 * r ** 2) + 1.5 + 1.2 * (MMD_weight - 1)
-    #ds  = -2 * r + 1.5*r **2 + 1.5
-    #tf.print(ds.shape)
-    # return 1 / normSigma * (SigmaTsq) * lam * K.sum(dm ** 2 * K.sum(W ** 2, axis=1), axi0s=1)
-    # grow one level in front
+
+    #f = tf.case({tf.less(tf.abs(x), alp): lambda: 10,
+    #             tf.math.logical_and(tf.greater_equal(x, alp), tf.math.less(x, alp)): lambda: 0,
+    #             tf.greater(x, alp): lambda: 10},
+    #                            exclusive=True)
+
+    #ds = MMD_weight * (-2 * r + 1.5 * r ** 2) + 1.5 + 1.2 * (MMD_weight - 1)
+
+    #tf.where([(tf.less(tf.abs(x), alp): lambda: 10,
+    #             tf.math.logical_and(tf.greater_equal(x, alp), tf.math.less(x, alp)): lambda: 0,
+    #             tf.greater(x, alp): lambda: 10], [10, 0, 10])
+    ds= 500*tf.math.square(tf.math.abs(alp-r)) *  tf.dtypes.cast(tf.less(r, alp)  , tf.float32) + \
+        0 * tf.dtypes.cast(tf.math.logical_and(tf.greater_equal(r, alp), tf.less(r, 1)), tf.float32) + \
+        (r**2-1)  * tf.dtypes.cast(tf.greater_equal(r, 1), tf.float32) + 0.1
+    #ds = pot(0.1, r)
     S_0W = tf.einsum('akl,lj->akj', du, U)
     S_1W = tf.einsum('akl,lj->akj', dm, W)  # N_batch x N_input ??
     # tf.print((S_1W).shape) #[None, 120]
@@ -385,6 +232,10 @@ def DCAE_loss(x, x_decoded_mean):  # attempt to avoid vanishing derivative of si
     diff_tens = tf.einsum('akl,alj->akj', diff_tens, S_0W)
     # tf.Print(K.sum(diff_tens ** 2))
     return 1 / normSigma * (SigmaTsq) * lam *(K.sum(diff_tens ** 2))
+
+#1000.0*  np.less(r, alp).astype(int)  + \
+#        0* (np.logical_and(np.greater_equal(r, alp), np.less(r, 1))).astype(int) + \
+#        1000.0* np.greater_equal(r, 1).astype(int)
 
 
 #mmd staff TODO: try approximation for this
@@ -403,8 +254,8 @@ def compute_mmd(x, y):   # [batch_size, z_dim] [batch_size, z_dim]
 def loss_mmd(x, x_decoded_mean):
     batch_size = K.shape(z_mean)[0]
     latent_dim = K.int_shape(z_mean)[1]
-    true_samples = K.random_normal(shape=(batch_size, latent_dim), mean=0.0, stddev=0.5)
-    #true_samples = K.random_uniform(shape=(batch_size, latent_dim), minval = -1., maxval = 1.0)
+    true_samples = K.random_normal(shape=(batch_size, latent_dim), mean=0.0, stddev=1.)
+    #true_samples = K.random_uniform(shape=(batch_size, latent_dim), minval = -1.5, maxval = 1.5)
     #true_samples = K.random_uniform(shape=(batch_size, latent_dim), minval=0.0, maxval=1.0)
     return compute_mmd(true_samples, z_mean)
 
@@ -415,21 +266,36 @@ def mean_square_error_NN(y_true, y_pred):
     # return 0.5 * (tf.multiply(weightedN, 1/SigmaTsq))
     return  tf.multiply(msew, normSigma * 1/SigmaTsq ) # TODO Sigma -denomiator or nominator? try reverse, schek hpw sigma computed in UMAP
 
+
 def ae_loss(weight, MMD_weight_lst):
     def loss(x, x_decoded_mean):
         msew = mean_square_error_NN(x, x_decoded_mean)
-        return msew + 10*(2-MMD_weight) * loss_mmd(x, x_decoded_mean) + coeffCAE * DCAE_loss(x, x_decoded_mean)
+        #return msew + (2-MMD_weight) * loss_mmd(x, x_decoded_mean) + coeffCAE * DCAE_loss(x, x_decoded_mean)
+        return msew +  (1-MMD_weight) *loss_mmd(x, x_decoded_mean) + coeffCAE * DCAE_loss(x, x_decoded_mean)
         # return K.mean(msew)
     return loss
+'''
+def ae_loss(weight, MMD_weight_lst):
+    def loss(x, x_decoded_mean):
+        msew = mean_square_error_NN(x, x_decoded_mean)
+        return msew #+  loss_mmd(x, x_decoded_mean)# + coeffCAE * DCAE3D_loss(x, x_decoded_mean)
+        # return K.mean(msew)
+    return loss
+'''
+
 
 autoencoder.summary()
 import tensorflow_addons as tfa
 #opt = tfa.optimizers.RectifiedAdam(lr=1e-3)
 #opt = tf.keras.optimizers.SGD(learning_rate=0.01, decay=0, momentum=0.9, nesterov =False)
+'''
 opt=tf.keras.optimizers.Adam(
     learning_rate=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-07, amsgrad=False
 )
+#autoencoder.compile(optimizer=opt, loss=ae_loss(MMD_weight, MMD_weight_lst), metrics=[DCAE_loss, loss_mmd,  mean_square_error_NN])
 autoencoder.compile(optimizer=opt, loss=ae_loss(MMD_weight, MMD_weight_lst), metrics=[DCAE_loss, loss_mmd,  mean_square_error_NN])
+
+
 
 start = timeit.default_timer()
 history = autoencoder.fit([aFrame, Sigma], aFrame,
@@ -506,13 +372,20 @@ Html_file.close()
 #checkpoint = ModelCheckpoint('/media/grines02/vol1/Box Sync/Box Sync/CyTOFdataPreprocess/models/model_knn_ae{epoch:08d}.h5',
 #                             save_freq=20*batch_size,
 #                            save_best_only=False)
-autoencoder.compile(optimizer=opt, loss=ae_loss(MMD_weight, MMD_weight_lst),
-                    metrics=[DCAE_loss, loss_mmd,  mean_square_error_NN])
+#autoencoder.compile(optimizer=opt, loss=ae_loss(MMD_weight, MMD_weight_lst),
+ #                   metrics=[DCAE_loss, loss_mmd,  mean_square_error_NN])
+ '''
+opt=tf.keras.optimizers.Adam(
+    learning_rate=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-07, amsgrad=False
+)
+autoencoder.compile(optimizer=opt, loss=ae_loss(MMD_weight, MMD_weight_lst), metrics=[DCAE_loss, loss_mmd,  mean_square_error_NN])
+
 
 from tensorflow.keras.callbacks import Callback
+save_period = 100
 class plotCallback(Callback):
     def on_epoch_end(self, epoch, logs=None):
-        if epoch % 200 == 0 or epoch in range(50):
+        if epoch % save_period  == 0 or epoch in range(200):
             z=encoder.predict([aFrame,  Sigma])
             fig = plot3D_cluster_colors(z, lbls=lbls)
             html_str = to_html(fig, config=None, auto_play=True, include_plotlyjs=True,
@@ -533,17 +406,17 @@ history_multiple = autoencoder.fit([aFrame, Sigma], aFrame,
 stop = timeit.default_timer()
 z = encoder.predict([aFrame,  Sigma])
 print(stop - start)
-st=10
+st=1
 fig01 = plt.figure();
 plt.plot(history_multiple.history['loss'][st:]);
 plt.title('loss')
-fig0 = plt.figure();
+fig02 = plt.figure();
 plt.plot(history_multiple.history['DCAE_loss'][st:]);
 plt.title('DCAE_loss')
 fig03 = plt.figure();
 plt.plot(history_multiple.history['loss_mmd'][st:]);
 plt.title('loss_mmd')
-fig03 = plt.figure();
+fig04 = plt.figure();
 plt.plot(history_multiple.history['mean_square_error_NN'][st:]);
 plt.title('mean_square_error')
 fig = plot3D_cluster_colors(z, lbls=lbls)
